3992bc6c0ab8b66e20509cff6a5b14ddaf287b8e
Thiago Rios de Siqueira <thiago.sqr@gmail.com>
2015-11-25 17:18:40 -0200

    sortedseq prop

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,17 +19,57 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h))==m
   }
 
-  property("deleteMinimum") = forAll { a: A =>
+  property("meld") = forAll { (a: A, aa: A) =>
+    val h1 = insert(a,empty)
+    val h2 = insert(aa,empty)
+
+    val h3 = meld(h1, h2)
+    val min = findMin(h3)
+
+    (min == a || min == aa) && !isEmpty(h3)
+
+  }
+
+  property("empty") = forAll { a: A =>
     val h = insert(a,empty)
     isEmpty(deleteMin(h))
   }
+
+  property("sortedseq") = forAll { h: H =>
+
+
+
+    def insertList(h: H, l: List[A]): H = l match {
+      case List() => h
+      case x :: xs => insertList(insert(x,h), xs)
+    }
+
+    def isSequenced(h: H, lastMin: A): Boolean = {
+
+      val curMin = findMin(h)
+
+      if(isEmpty(h)) true
+      else if(lastMin > curMin) false
+      else isSequenced(deleteMin(h), curMin)
+    }
+
+    val l = arbitrary[A].map
+
+    val h = insertList(empty,l);
+    val a = findMin(h)
+
+    isSequenced(deleteMin(h), a)
+
+  }
+
+
 
 
   val emptyCons = const(empty)
 
   lazy val genHeap: Gen[H] = for {
     a <- arbitrary[A]
-    h <- oneOf(emptyCons, genHeap)
+    h <- emptyCons
   } yield insert(a,h)
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
